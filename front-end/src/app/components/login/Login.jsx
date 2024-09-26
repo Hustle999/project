8978dfcac28abd863aff79efadb5e80fb322be4e
Төгөлдөр Гантөмөр <24LP4099@G401-CC0106.local>
2024-09-26 08:57:48 +0800

    page shiljdeg bolson

---
+++ front-end/src/app/components/login/Login.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Link from "next/link";
+
+export const Login = () => {
+  return (
+    <main>
+      <Link href="register">
+        <button>
+          <div>Hi</div>
+        </button>
+      </Link>
+    </main>
+  );
+};
