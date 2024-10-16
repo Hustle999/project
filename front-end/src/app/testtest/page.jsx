75f6e3ddcc6d87db12e420b3861be15ad7f55692
Төгөлдөр Гантөмөр <24LP4099@G401-CC0106.local>
2024-10-16 10:44:34 +0800

    tester page neesen

---
+++ front-end/src/app/testtest/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Tester } from "../components/tester/Tester";
+
+export const Test = () => {
+  return (
+    <main>
+      <Tester />
+    </main>
+  );
+};
+export default Test;
